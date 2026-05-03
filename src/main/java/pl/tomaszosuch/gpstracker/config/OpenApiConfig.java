e5d1a4c1d15b6a1067ea304bc808b60c624aef43
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-05-04 00:47:05 +0200

    fix(): poprawki do projektu

--- src/main/java/pl/tomaszosuch/gpstracker/config/OpenApiConfig.java
+++ src/main/java/pl/tomaszosuch/gpstracker/config/OpenApiConfig.java
@@ -1,9 +1,7 @@
 package pl.tomaszosuch.gpstracker.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,13 +25,7 @@ public class OpenApiConfig {
                                 - Wysyłanie i pobieranie danych GPS
                                 - Obsługa bardzo dużej liczby równoczesnych urządzeń
                                 """)
-                        .version("1.0.0")
-                        .contact(new Contact()
-                                .name("GPS Tracker Team")
-                                .email("contact@gpstracker.com"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                        .version("1.0.0"))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Lokalny serwer developerski"),
                         new Server().url("http://app:8080").description("Serwer Docker")
